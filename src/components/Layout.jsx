0585e7c95c6526a91a71786d3cc15919bd54e207
Wanchai7 <wanchai.view47@gmail.com>
2025-12-08 10:36:02 +0700

    สร้าง routers, pages

---
+++ src/components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {Outlet} from "react-router";
+import Header from './Header';
+import Footer from './Footer';
+
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-gradien-to-r from-blue-500 to-purple-600">
+        <header>
+            <Header/>
+        </header>
+
+        <main className="grow flex items-center justify-center min-h-screen mx-auto pt-5 mt-5 sm:p-6 lg:p-8">
+        <Outlet/>
+        </main>
+
+        <footer>
+            <Footer/>
+        </footer>
+    </div>
+  )
+}
+
+export default Layout
---
+++ src/routers/Router.jsx
@@ -0,0 +1,27 @@
+import { createBrowserRouter } from "react-router";
+import Layout from "../components/Layout";
+import Home from "../pages/Home";
+import Edit from "../pages/Edit";
+import Create from "../pages/Create";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import PostDetail from "../pages/PostDetail";
+import Author from "../pages/Author"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "edit/:id", element: <Edit /> },
+      { path: "create", element: <Create /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      {path:"post/:id", element:<PostDetail/>},
+      {path:"author/id", element:<Author/>}
+    ],
+  },
+]);
+
+export default router;
