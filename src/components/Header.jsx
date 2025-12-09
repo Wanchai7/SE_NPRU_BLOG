39cec00fba9fb2dc9174671d6b02d9a1b6506857
Wanchai7 <wanchai.view47@gmail.com>
2025-12-09 09:45:10 +0700

    แก้ไขหน้า Header.jsx ให้กดตรง SE NPRU Blog สามารถกดแล้วไปหน้า Home ได้

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,26 @@ import React from 'react'
 const Header = () => {
   const menuItems = [
     { text: "Login", link: "/login" },
-    { text: "Register", link: "/register" },
-    
+    { text: "Register", link: "/register" }, 
   ];
+
+  const home = [
+    { text: "SE NPRU Bolg", link: "/home" },
+  ]
   
   return (
     <div className="navbar bg-base-100 shadow-sm">
+      {/* ส่วนของ SE NPRU Blog */}
       <div className="navbar-start">
-        <a
-          onClick={() => navigate("/")}
-          className="btn btn-ghost normal-case text-2xl font-bold cursor-pointer"
-        >
-          SE NPRU Blog
-        </a>
+        <ui className="menu menu-horizontal px-6">
+          {home.map((item) => (
+            <li>
+              <a href={item.link} className="link link-hover">{item.text}</a>
+            </li>
+          ))}
+        </ui>
       </div>
-
+      {/* ส่วนของ Login Register */}
       <div className="navbar-end">
         <ui className="menu menu-horizontal px-1">
           {menuItems.map((item) => (
